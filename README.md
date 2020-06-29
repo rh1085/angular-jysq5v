225275be34ea3cc8e04c989eb4ce76d38ab64e99
rh1085 <rh1085@txstate.edu>
2020-06-29 15:29:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jysq5v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jysq5v)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Maui Getaway',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    image: 'https://www.hawaiimagazine.com/sites/default/files/field/image/Destination%20Maui%20Header%201200x800.jpg'
+  },
+  {
+    name: 'Maui Escape',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    image: 'https://www.hawaiimagazine.com/sites/default/files/field/image/Destination%20Maui%20Header%201200x800.jpg'
+  },
+  {
+    name: 'Hilton Maui',
+    price: 299,
+    description: '',
+    image: 'https://www.hawaiimagazine.com/sites/default/files/field/image/Destination%20Maui%20Header%201200x800.jpg'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
